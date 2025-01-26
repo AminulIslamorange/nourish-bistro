e64dfa2a5febcb47c944398e72add096c49b2454
AminulIslamorange <aminulislamorange@gmail.com>
2025-01-26 18:17:09 +0600

    custom hook added

--- src/Pages/Home/PopularMenu/PopularMenu.jsx
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,17 +1,22 @@
-import { useEffect, useState } from "react";
+
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import MenuItem from "../../Shared/MenuItem/MenuItem";
+import useMenu from "../../../hooks/useMenu";
 
 
 const PopularMenu = () => {
-    const [menu,setMenu]=useState([]);
-    useEffect(()=>{
-        fetch('menu.json')
-        .then(res=>res.json())
-        .then(data=>{
-            const popularItem=data.filter(item=>item.category ==='popular')
-            setMenu(popularItem)})
-    },[])
+    const [menu]=useMenu();
+    const popularItem=menu.filter(item=>item.category ==='popular')
+
+
+    // const [menu,setMenu]=useState([]);
+    // useEffect(()=>{
+    //     fetch('menu.json')
+    //     .then(res=>res.json())
+    //     .then(data=>{
+    //         const popularItem=data.filter(item=>item.category ==='popular')
+    //         setMenu(popularItem)})
+    // },[])
     return (
        <section className="mb-12 text-center">
         <SectionTitle subHeading={'---Check it out---'}heading={'FROM OUR MENU'}>
@@ -20,7 +25,7 @@ const PopularMenu = () => {
 
         <div className="grid md:grid-cols-2 gap-4">
             {
-            menu.map(item=><MenuItem 
+            popularItem.map(item=><MenuItem 
                 key={item._id}
                  item={item}
                 ></MenuItem>)
