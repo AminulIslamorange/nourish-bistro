42dbffcdeb6505e09310317b4cd5595dc3305408
AminulIslamorange <aminulislamorange@gmail.com>
2025-02-02 17:54:42 +0600

    order page cover img added

--- src/Pages/Menu/Menu/Menu.jsx
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -13,7 +13,7 @@ import soupImg from '../../../assets/menu/soup-bg.jpg'
 
 const Menu = () => {
     const [menu] = useMenu();
-    const drinks = menu.filter(item => item.category === 'drinks');
+    
     const soup = menu.filter(item => item.category === 'soup');
     const salad = menu.filter(item => item.category === 'salad');
     const pizza = menu.filter(item => item.category === 'pizza');
---
+++ src/Pages/Order/Order/Order.jsx
@@ -0,0 +1,13 @@
+import orderCoverpageImg from '../../../assets/shop/banner2.jpg'
+import Cover from '../../Shared/Cover/Cover';
+
+const Order = () => {
+    return (
+        <div>
+            <Cover img={orderCoverpageImg} title='OUR SHOP' details='Would you like to try a dish?'></Cover>
+            
+        </div>
+    );
+};
+
+export default Order;
