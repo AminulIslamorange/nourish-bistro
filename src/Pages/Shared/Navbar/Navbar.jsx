382d92876c64195556312419c02e67993cced591
AminulIslamorange <aminulislamorange@gmail.com>
2025-02-24 20:15:14 +0600

    cart item work started

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,67 +1,74 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../Provider/AuthProvider";
+import { FaCartShopping } from "react-icons/fa6";
 
 
 const Navbar = () => {
-  const {user,logOut}=useContext(AuthContext);
-  const handleLogOut=()=>{
+  const { user, logOut } = useContext(AuthContext);
+  const handleLogOut = () => {
     logOut()
-    .then(()=>{})
-    .catch(error=>console.error(error))
+      .then(() => { })
+      .catch(error => console.error(error))
   }
-    const navOption=<>
+  const navOption = <>
     <li><Link to='/'>Home</Link></li>
     <li><Link to='/menu'>Our Menu</Link></li>
     <li><Link to='/order/salad'>Order</Link></li>
-    
+    <li><Link to='/'>
+      <button className="btn">
+      <FaCartShopping />
+        <div className="badge badge-secondary">+0</div>
+      </button></Link></li>
+
     {
-    user?<><span className="text-xl text-white">{user?.displayName}</span>
-    <button onClick={handleLogOut} className="btn btn-ghost">LogOut</button>
-    </>:
-    <><li><Link to='login'>Login</Link></li> </>
+      user ? <>
+        <span className="text-xl text-white">{user?.displayName}</span>
+        <button onClick={handleLogOut} className="btn btn-ghost">LogOut</button>
+      </> :
+        <><li><Link to='login'>Login</Link></li> </>
     }
-    </>
-    return (
-        <div>
-        <div className="navbar fixed z-30 bg-opacity-30 bg-black text-white mx-auto max-w-screen-xl">
-<div className="navbar-start">
-<div className="dropdown">
-  <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-5 w-5"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor">
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M4 6h16M4 12h8m-8 6h16" />
-    </svg>
-  </div>
-  <ul
-    tabIndex={0}
-    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-    {navOption}
-    
-  </ul>
-</div>
-<a className="btn btn-ghost text-xl">NOURISH BISTRO<br/>RESTAURANT</a>
-</div>
-<div className="navbar-center hidden lg:flex">
-<ul className="menu menu-horizontal px-1">
-  {navOption}
-</ul>
-</div>
-<div className="navbar-end">
-<a className="btn">Button</a>
-</div>
-</div>
-        
+  </>
+  return (
+    <div>
+      <div className="navbar fixed z-30 bg-opacity-30 bg-black text-white mx-auto max-w-screen-xl">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+              {navOption}
+
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">NOURISH BISTRO<br />RESTAURANT</a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            {navOption}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+
     </div>
-    );
+  );
 };
 
 export default Navbar;
--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -1,7 +1,11 @@
 
 
+
 const FoodCard = ({ item }) => {
     const { name, image, recipe, price } = item;
+    const handleAddToCart=(food)=>{
+        console.log(food)
+    }
     return (
         <div className="card bg-base-100 w-96 shadow-xl relative">
             <figure>
@@ -14,7 +18,8 @@ const FoodCard = ({ item }) => {
                 <h2 className="card-title justify-center">{name}</h2>
                 <p className="justify-start">{recipe}</p>
                 <div className="card-actions justify-center">
-                    <button className="btn btn-outline border-0 border-b-4  mt-2 text-center text-[#BB8506]">Add To Cart</button>
+                    <button onClick={()=>{handleAddToCart(item)}}
+                     className="btn btn-outline border-0 border-b-4  mt-2 text-center text-[#BB8506]">Add To Cart</button>
                 </div>
             </div>
         </div>
