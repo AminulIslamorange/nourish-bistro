e7a73b51996f8b2ea78a5b348cb6eb5dd56d7a55
AminulIslamorange <aminulislamorange@gmail.com>
2025-03-22 16:08:16 +0600

    stripe install and function added

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,10 +1,16 @@
+import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
-
-
+import { Elements } from "@stripe/react-stripe-js";
+import CheckOutForm from "./checkOutForm";
+// ToDo need a key
+const stripePromise = loadStripe(import.meta.env.VITE_PAYEMENT_API);
 const Payment = () => {
     return (
         <div>
             <SectionTitle heading={'PAYMENT'}></SectionTitle>
+            <Elements stripe={stripePromise}>
+      <CheckOutForm/>
+    </Elements>
         </div>
     );
 };
